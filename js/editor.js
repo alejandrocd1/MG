373fbdedf45aefdf433c87a94efb45b69a5a20d4
alejandrocd1 <chacondemilita@gmail.com>
2026-05-07 15:19:38 +0100

    Update editor.js

--- js/editor.js
+++ js/editor.js
@@ -1,1 +1,20 @@
+let expr = "";
 
+function addSymbol(s) {
+  expr += s;
+  updateDisplay();
+}
+
+function clearExpr() {
+  expr = "";
+  updateDisplay();
+}
+
+function backspace() {
+  expr = expr.slice(0, -1);
+  updateDisplay();
+}
+
+function updateDisplay() {
+  document.getElementById("expression").innerText = expr;
+}
