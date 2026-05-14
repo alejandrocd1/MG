8e0f4cd5d8e0de4cbc6a4687ea5ed2ad66183103
alejandrocd1 <chacondemilita@gmail.com>
2026-05-14 17:09:39 +0100

    Refactor setup and enhance drawing functions

--- js/sketch.js
+++ js/sketch.js
@@ -1,5 +1,7 @@
 function setup() {
-  let container = document.getElementById("canvasContainer");
+
+  let container =
+    document.getElementById("canvasContainer");
 
   let canvas = createCanvas(
     container.offsetWidth,
@@ -10,10 +12,98 @@ function setup() {
 }
 
 function draw() {
+
   background(20);
+
+  translate(width / 2, height / 2);
+
+  drawGrid();
+
+  drawAxes();
+
+  drawF();
+
+  drawG();
+}
+
+/* CUADRÍCULA */
+
+function drawGrid() {
+
+  stroke(60);
+
+  strokeWeight(1);
+
+  for (let x = -width; x < width; x += 25) {
+    line(x, -height, x, height);
+  }
+
+  for (let y = -height; y < height; y += 25) {
+    line(-width, y, width, y);
+  }
+}
+
+/* EJES */
+
+function drawAxes() {
 
   stroke(255);
 
-  line(width / 2, 0, width / 2, height);
-  line(0, height / 2, width, height / 2);
+  strokeWeight(2);
+
+  // eje X
+  line(-width, 0, width, 0);
+
+  // eje Y
+  line(0, -height, 0, height);
+}
+
+/* f(x)=x² */
+
+function drawF() {
+
+  stroke(0, 150, 255);
+
+  strokeWeight(3);
+
+  noFill();
+
+  beginShape();
+
+  for (let x = -5; x <= 5; x += 0.1) {
+
+    let y = x * x;
+
+    vertex(
+      x * 40,
+      -y * 20
+    );
+  }
+
+  endShape();
+}
+
+/* g(x)=-0.5x²+6 */
+
+function drawG() {
+
+  stroke(255, 80, 80);
+
+  strokeWeight(3);
+
+  noFill();
+
+  beginShape();
+
+  for (let x = -5; x <= 5; x += 0.1) {
+
+    let y = -0.5 * x * x + 6;
+
+    vertex(
+      x * 40,
+      -y * 20
+    );
+  }
+
+  endShape();
 }
